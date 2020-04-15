073aaf821b5f10408399a633a3d121a018965318
Camen <camenpihor@gmail.com>
2020-04-15 12:12:57 -0700

    Persist training data to wildfire/ directory instead of /nobackupp10/ since I don't have access

--- bin/noaa_l2_training_data.py
+++ bin/noaa_l2_training_data.py
@@ -1,4 +1,3 @@
-import os, sys
 from datetime import datetime
 import glob
 
@@ -7,14 +6,13 @@ import numpy as np
 from mpi4py import MPI
 #import h5py
 
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from wildfire import wildfire
 from wildfire.goes import utilities, scan
 
 # Open MPI communiciation
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank() # process rank
-size = comm.Get_size() # number of processes 
+size = comm.Get_size() # number of processes
 name = MPI.Get_processor_name()
 
 # 2019-09-07T01:50:13.1Z
@@ -30,14 +28,14 @@ patch_size = 32
 fire_path = '/nobackupp10/tvandal/data/goes16/ABI-L2-FDCF/{}/{}/{}/'
 ABI_directory = '/nex/datapool/geonex/public/GOES16/NOAA-L1B/'
 
-training_directory = '/nobackupp10/tvandal/wildfire/.tmp/training-data/abi-noaal1b-F/'
+training_directory = './.tmp/training-data/abi-noaal1b-F/'
 
 if not os.path.exists(training_directory):
     os.makedirs(training_directory)
 
 def extract_patches_2d(arr, height, width, stride):
     '''
-    Given a Tensor of shape (H, W, ...) extract patches of 
+    Given a Tensor of shape (H, W, ...) extract patches of
     size (height, width) with stride
     '''
     assert len(arr.shape) >= 2
@@ -64,8 +62,8 @@ def get_file_examples(fire_file):
     start_time = datetime.strptime(start_time, DATETIME_FORMAT)
     abi_files = utilities.list_local_files(ABI_directory, satellite, 'F', start_time)
     abi_scan = scan.read_netcdfs(local_filepaths=abi_files)
-    abi_scan_2km = abi_scan.rescale_to_2km() 
-    
+    abi_scan_2km = abi_scan.rescale_to_2km()
+
     rads = []
     band16 = abi_scan_2km['band_16']
     x_2km = band16.dataset.x.values
@@ -73,9 +71,9 @@ def get_file_examples(fire_file):
     for band, s in abi_scan_2km.iteritems():
         rads.append(s.normalize())
         rads[-1] = rads[-1].assign_coords(x=x_2km, y=y_2km)
-        
+
     scan_rads = xr.concat(rads, 'band')
-    
+
     alldata = np.concatenate([scan_rads.values, ds_fire.Temp.values[np.newaxis]], 0)
     alldata = np.transpose(alldata, (1,2,0))
     alldata_patches = extract_patches_2d(alldata, patch_size, patch_size, patch_size)
@@ -104,7 +102,7 @@ inputs = examples[:,:,:,:16].astype(np.float32)
 labels = examples[:,:,:,16].astype(np.float32)
 
 example_file = os.path.join(training_directory, '%03i.nc' % rank)
-dsout = xr.Dataset({"abi": xr.DataArray(inputs), 
+dsout = xr.Dataset({"abi": xr.DataArray(inputs),
                     "fire_temp": xr.DataArray(labels)})
 dsout.to_netcdf(example_file)
 print(f"Rank: {rank} -- Saved examples to file: {example_file}")
